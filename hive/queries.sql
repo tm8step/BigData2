1e400150685f5b50c9655bcb26fe4ddd91153a7d
tm8step <106827963+tm8step@users.noreply.github.com>
2026-08-23 11:21:30 -0500

    Insert Hive queries for data validation and analysis
    Added representative Hive queries for data analysis.

--- hive/queries.sql
+++ hive/queries.sql
@@ -1,9 +1,20 @@
--- DSC 650 Portfolio Starter
--- Replace these comments with representative Hive queries from your final project.
---
--- Choose queries that demonstrate meaningful work rather than every query you ran.
+-- These were the Queries used to validate the Hive table
 
--- Example:
--- SELECT ...
--- FROM ...
--- WHERE ...;
+-- This query finds the average of the taxes column, rounded to  2 decimals.
+SELECT ROUND(AVG(taxes), 2) FROM econdata_ilwi;
+
+-- Result 7226.2
+
+-- This query finds the maximum housing for each state, IL and WI
+SELECT state_abv, MAX(housing) AS max_housing
+FROM econdata_ilwi 
+GROUP BY state_abv;
+
+-- Result:  
+-- IL      19431.04
+-- WI      12422.97
+
+--  This query counts the total number of data entries.
+SELECT COUNT(*) FROM econdata_ilwi;
+
+-- Result:  174
